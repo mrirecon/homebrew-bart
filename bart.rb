4d00b5d8c4f23d9f0cfd87562b896e77f0a08de4
Maximilian Diefenbach <maximilian.diefenbach@tum.de>
2018-10-17 17:48:00 -0700

    updated bart.rb to latest bart + dependencies versions

--- bart.rb
+++ bart.rb
@@ -1,23 +1,22 @@
 class Bart < Formula
   desc "Image-reconstruction toolbox for computational MRI"
   homepage "https://mrirecon.github.io/bart/"
-  version "0.3.01"
+  version "0.4.03"
   url "https://github.com/mrirecon/bart/archive/v#{version}.tar.gz"
-  sha256 "3c34b28ce3151f7f1ffe23b16fb0014123668b22e316b842e32ec125a079d09a"
 
   option "with-cuda", "Do GPU computing with CUDA"
   option "with-serial", "Do serial computing by turning off OpenMP"
 
   depends_on "fftw"
-  depends_on "gcc"
-  depends_on "homebrew/science/openblas"
-  depends_on "homebrew/versions/libpng12"
+  depends_on "gcc@8"
+  depends_on "openblas"
+  depends_on "libpng"
 
   def install
-    ENV["CC"] = "gcc-5"
+    ENV["CC"] = "gcc-8"
     ENV["MACPORTS"] = "0"
     ENV["CUDA"] = "1" if build.with? "cuda"
-    ENV["OMP"] = "0" if build.with? "serial"
+    ENV["OMP"] = "1" if build.with? "serial"
     system "make"
     bin.install "bart"
   end
